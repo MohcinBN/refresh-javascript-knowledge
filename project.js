cd83dfbbc8a4ade6d15e9213a824024e4b670a6d
MohcinBN <rapmabiz@gmail.com>
2022-12-03 12:16:57 +0100

    second commit

---
+++ project.js
@@ -0,0 +1,9 @@
+// get the UI vriables
+const form = document.querySelector('#employee-form');
+const employeeList = document.querySelector('ul.collection-employee');
+const removeEmployee = document.querySelector('.delete-all');
+const employeeInput = document.querySelector('#full_name');
+
+const deleteItem = document.querySelector('.delete-employee');
+
+//console.log(employeeInput);
